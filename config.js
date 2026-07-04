6c0c9321f259c37792451874120026f5aa1b213e
Bastián Encina <bastianProject09@gmail.com>
2026-07-03 23:21:31 -0400

    Update dashboard configuration files

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Configuración de Supabase
-const SUPABASE_URL = 'https://TU_PROYECTO.supabase.co';
-const SUPABASE_KEY = 'tu-anon-key';
+const SUPABASE_URL = 'https://ccevknouobhmixdcqiac.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_Bj2bEZSXHYNWUm0FDviVDA_KJ_es8DQ';
